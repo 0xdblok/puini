465b7d2cda1fa461d521d19253750ef7c816c2a8
0xdblok <dblok0x@gmail.com>
2024-04-17 17:44:05 +0200

    ee

--- app/component/footer.js
+++ app/component/footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
             height={30}
           />
         </a>
-        <a href="">
+        <a href="https://t.me/+dL1lnfU9hF5hYWQ1">
           {" "}
           <Image
             className="lg:w-[40px] lg:h-[40px]"
