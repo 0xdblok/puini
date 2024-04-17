befd0cea9451703f2e99e132185b6d82ab084c30
0xdblok <dblok0x@gmail.com>
2024-04-17 18:32:48 +0200

    ee

--- app/component/hero.js
+++ app/component/hero.js
@@ -6,19 +6,24 @@ export default function Hero() {
     <div>
       <div className="flex-none   md:flex md:justify-center md:gap-32 pt-8">
         {" "}
-        <div className="flex justify-center md:w-1/2">
+        <div className=" md:w-1/2">
           {" "}
-          <Image
-            className=" rounded-full bg-[#F5E6E8]"
-            src={hero}
-            alt=" "
-            width={350}
-            height={350}
-          />
+          <div className=" flex justify-center">
+            <Image
+              className=" flex justify-center rounded-full borded-4 border-white  bg-[#F5E6E8]"
+              src={hero}
+              alt=" "
+              width={350}
+              height={350}
+            />
+          </div>
+          <h1 className="text-xl font-serif text-center text-gray-700">
+            PINK PUNI
+          </h1>
         </div>
         <div className=" md:flex md:w-1/2 md:items-center ">
           {" "}
-          <h1 className="text-xl font-serif text-center text-gray-700">
+          <h1 className="text-xl pt-6 md:pt-0 font-serif text-center text-gray-700">
             Hiùm with shades on, holding a magnifying glass, symbolizing
             research and exploration
           </h1>
